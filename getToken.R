f338c9270dcc5b0bf7b496d6e2f608730ea543c2
Annat Haber <annat22@gmail.com>
2021-08-20 17:11:48 -0400

    add getToken

---
+++ getToken.R
@@ -0,0 +1,7 @@
+library(httr)
+getToken <- function(username) {
+  if (!any(grepl("climb_pwd", unlist(keyring::key_list())))) keyring::key_set("climb_pwd")
+  tokenreq <- httr::GET("http://climb-admin.azurewebsites.net/api/token",
+                  authenticate(username, keyring::key_get("climb_pwd")))
+  paste0("Bearer ", content(tokenreq)$access_token)
+}
