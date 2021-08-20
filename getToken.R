3a1573ba01e085321716a4f71a4ca134835f045f
Annat Haber <annat22@gmail.com>
2021-08-20 19:51:59 -0400

    bla

--- getToken.R
+++ getToken.R
@@ -1,6 +1,7 @@
 getToken <- function(username) {
+  library(httr)
   if (!any(grepl("climb_pwd", unlist(keyring::key_list())))) keyring::key_set("climb_pwd")
-  tokenreq <- httr::GET("http://climb-admin.azurewebsites.net/api/token",
+  tokenreq <- GET("http://climb-admin.azurewebsites.net/api/token",
                   authenticate(username, keyring::key_get("climb_pwd")))
-  paste0("Bearer ", httr::content(tokenreq)$access_token)
+  paste0("Bearer ", content(tokenreq)$access_token)
 }
