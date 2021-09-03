7710fdc418e2f7e4202b607190b42e6ccd7f71be
Annat Haber <annat22@gmail.com>
2021-09-03 16:27:56 -0400

    nulgeneralized key2name

--- key2name.R
+++ key2name.R
@@ -3,9 +3,9 @@
 ## Authentication is done by retrieving a temporary token with getToken.R
 
 source("https://raw.github.com/TheJacksonLaboratory/ClimbR/master/climbRequest.R")
-key2name <- function(key, field, climb_username=NULL) {
-  facet <- paste0("vocabulary/", field)
-  qs <- paste0("key=",key)
+key2name <- function(key, facet, field=NULL, climb_username=NULL) {
+  if (facet=="vocabularies") facet <- paste0("vocabulary/", field)
+  qs <- paste0(str_to_sentence(gsub("s", "", facet)),"key=", key)
   res <- content(climbRequest("GET", facet, qs, climb_username=climb_username))
   res$data$items[[1]]$name
   }
