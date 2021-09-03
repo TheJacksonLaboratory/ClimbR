8f759acfa5aef0f8479719bc08e9277e3cdd6ce1
Annat Haber <annat22@gmail.com>
2021-09-03 15:53:00 -0400

    null username

--- key2name.R
+++ key2name.R
@@ -3,9 +3,9 @@
 ## Authentication is done by retrieving a temporary token with getToken.R
 
 source("https://raw.github.com/TheJacksonLaboratory/ClimbR/master/climbRequest.R")
-key2name <- function(key, field, climb_username) {
+key2name <- function(key, field, climb_username=NULL) {
   facet <- paste0("vocabulary/", field)
   qs <- paste0("key=",key)
-  res <- content(climbRequest("GET", facet, qs, climb_username))
+  res <- content(climbRequest("GET", facet, qs, climb_username=climb_username))
   res$data$items[[1]]$name
   }
