f8c72cf8a9e56111991a6d50052a36a1aa31acc9
Annat Haber <annat22@gmail.com>
2021-09-05 13:15:37 -0400

    fixed getByName

--- getByName.R
+++ getByName.R
@@ -1,15 +1,19 @@
-getByName <- function(names, facet, climb_username=NULL) {
+getByName <- function(names, facet, climb_username=NULL, return_response=TRUE) {
   
   if (!is.null(climb_username)) token <- getToken(username)
   
+  respL <- list()
   itemsL <- list()
   for (nm in names) {
     qs <- paste0(str_to_sentence(gsub("s", "", facet)),"Name=", nm)
     url <- paste0("https://api.climb.bio/api/", facet, "?", qs)
     res <- GET(url, add_headers(.headers = c("Authorization" = token)))
-    itemsL[[nm]] <- content(res)$data$items[[1]]
+    cres <- content(res)$data$items
+    if(length(cres)==0) itemsL[[nm]] <- NA else itemsL[[nm]] <- cres[[1]]
+    if (return_response) respL[[nm]] <- res
     }
   
   df <- data.frame(do.call(rbind,itemsL))
-  df
+  
+  return(list(data=df, response=respL))
   }
