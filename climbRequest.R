095da866ca2a1107a8ec4ddb01b3788e34e80261
Annat Haber <annat22@gmail.com>
2021-09-22 20:09:34 -0400

    fixed token validation

--- climbRequest.R
+++ climbRequest.R
@@ -1,21 +1,24 @@
-## A generic API request
+## A generic API request following https://api.climb.bio/docs/index.html
 ## output is a response object given by package httr (https://httr.r-lib.org/reference/response.html)
 ## Authentication is done by retrieving a temporary token with getToken.R
-## queries are provided as a list of key-value pairs added to the default of PageSize and PageNumber
-## PageSize indicates how many items per page to request
-## PageNumber indicates how many pages to request
+## queries are provided as a list of key-value pairs,e.g, list(AnimalName="50101", AnimalNameSearchOptions="StartsWith")
 
 source("https://raw.github.com/TheJacksonLaboratory/ClimbR/master/getToken.R")
-climbRequest <- function(method, facetPath, queryList=NULL) {
+climbRequest <- function(method, endpointPath, queryList=NULL) {
   
-  # check if there is a valid token in the environment GET /api/Diagnostics 
-  test <- GET("https://api.climb.bio/api/Diagnostics", add_headers(Authorization = token))
-  
-  # GET token if not
-  if (test$status_code==401) token <- getToken()
+  # check if there is a token in the environment
+  if (class(try(token, silent=TRUE))=='try-error') {
+    # if there is none, get one
+    token <- getToken()
+    } else {
+    # if there is one already, check if it is valid 
+    test <- GET("https://api.climb.bio/api/Diagnostics", add_headers(Authorization = token))
+    # if existing one is not valid, get new one
+    if (test$status_code==401) token <- getToken()
+    }
   
   # build url including endpoint path and queries
-  url <- modify_url("https://api.climb.bio/", path=facetPath, query=queryList)
+  url <- modify_url("https://api.climb.bio/", path=endpointPath, query=queryList)
   
   # send request
   response <- VERB(method, url, add_headers(.headers = c(Authorization = token)))
