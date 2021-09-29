cd369c95664586943a11439b08bfcaeb12d32415
Annat Haber <annat22@gmail.com>
2021-09-29 16:31:10 -0400

    parsed response

--- climbRequest.R
+++ climbRequest.R
@@ -1,7 +1,10 @@
 ## A generic API request following https://api.climb.bio/docs/index.html
-## output is a response object given by package httr (https://httr.r-lib.org/reference/response.html)
 ## Authentication is done by retrieving a temporary token with getToken.R
 ## queries are provided as a list of key-value pairs,e.g, list(AnimalName="50101", AnimalNameSearchOptions="StartsWith")
+## output is a climb_api object including the full response as returned by package 
+## httr (https://httr.r-lib.org/reference/response.html) along with the following objects:
+## parsed data is returned as dataframe with records in rows and fields in columns
+## url, errors, and status code returned as strings
 
 source("https://raw.github.com/TheJacksonLaboratory/ClimbR/master/getToken.R")
 climbRequest <- function(method, endpointPath, queryList=NULL) {
@@ -21,7 +24,30 @@ climbRequest <- function(method, endpointPath, queryList=NULL) {
   url <- modify_url("https://api.climb.bio/", path=endpointPath, query=queryList)
   
   # send request
-  response <- VERB(method, url, add_headers(.headers = c(Authorization = token)))
+  resp <- VERB(method, url, add_headers(.headers = c(Authorization = token)))
+  resp[[9]] <- NULL # remove response element that has credentials
   
-  response
-  }
+  # parse response content 
+  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
+  data <- parsed$data
+  # make data structure consistent across unique vs paged lists responses
+  if(is.null(data$totalItemCount)) {
+    itemsL <- list(data)
+  } else {itemsL <- data$items}
+  # turn data object into dataframe, all strings and NA for missing values
+  items <- lapply(itemsL, function(it){
+    sapply(it, function(x){ifelse(is.null(x), NA_character_, as.character(x))})
+  })
+  items <- as.data.frame(do.call(rbind, items))
+
+  structure(
+    list(
+      response = resp,
+      status_code = resp$status_code,
+      url = resp$url,
+      error = parsed$errors,
+      data = items
+    ),
+    class = "climb_api"
+  )
+}
