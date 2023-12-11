35d9875169c5fc37bf685ca8e6a1f0ba1bb0f1cd
annat22 <annat22@gmail.com>
2023-12-11 12:46:43 -0500

    --

--- climbPOST.R
+++
@@ -1,9 +0,0 @@
-source("https://raw.github.com/TheJacksonLaboratory/ClimbR/master/climbRequest.R")
-
-climbPOST <- function(facet, endpoint, dataFrame=NULL, dataFile=NULL) {
-  if(is.null(dataFrame)) {data <- read.csv(dataFile, na.strings = c("", "NA"), as.is=TRUE)
-  } else data <- dataFrame
-  data.js <- toJSON(data)
-  resp <- climbRequest("POST", endpointPath=paste0("api/",facet, "/", endpoint), body=data.js)
-  
-}
